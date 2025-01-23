a82d64ee940b6e3324a722bd69f0a2794bd5ae7e
Lucas Fonseca Martins <148023244+lucastbucks@users.noreply.github.com>
2025-01-23 15:13:24 +0800

    index split

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 // index.js
+import ConversionTracking from './src/conversion-tracking.js';
 
-const ConversionTracking = (() => {
-    // Private variables and methods (not exposed to users)
-    let userData = {};
-
-    // Public function to track landing
-    const land = () => {
-        userData.landedAt = new Date().toISOString();
-        console.log("Landing recorded at:", userData.landedAt);
-        // You can also send this data to your server
-    };
-
-    // Expose public methods
-    return {
-        land,
-    };
-})();
-
-// Export the ConversionTracking object for usage
 export default ConversionTracking;
